878f50a673aa5e0afd7439f74e3e9333e8a455de
Nien <wolfzxcv@gmail.com>
2019-06-14 07:54:11 +0100

    first styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import Select from './components/Select'
+import List from './components/List'
 
-function App() {
+const header_bg = require('../src/images/header_bg.png')
+const icons_down = require('../src/images/icons_down.png')
+const btn_goTop = require('../src/images/btn_goTop.png')
+const all = require('./components/allData')
+
+const App = ({className}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={className}>
+      <header>
+      <img src={header_bg} />
+      <p>Kaohsiung Travel Guide</p>
       </header>
+      <Select />
+      <hr/><img className='icons_down' src={icons_down} />
+      {console.log(all.allData.map(i=> i.Name))} 
+      <List />
+      <img className='btn_goTop' src={btn_goTop} />
     </div>
-  );
+  )
 }
 
-export default App;
+App.propTypes = {
+  className: PropTypes.string
+}
+
+const StyledApp = styled(App)` 
+
+ header{
+  margin-top: 250px;
+  font-size: 40px;
+  color: #FFFFFF;
+  letter-spacing: 3.89px;
+
+ p{
+  text-align: center;
+ }  
+
+ img{ 
+  position: absolute; 
+  top: 0;
+  width: 100vw;
+  z-index: -1;
+  }
+}
+
+hr {
+	border: 2px dashed #559AC8;
+	width: 1200px;
+}
+
+.icons_down{
+  width: 20px;
+  height: 20px;
+  color: #559AC8 
+  display: flex;
+  position: relative;
+  top: -25px;
+  margin: 0 auto;
+  padding: 10px;
+  border: 2px solid #559AC8;  
+  border-radius: 50%;
+}
+
+.btn_goTop{
+	position: fixed;
+	right: 20px;
+	bottom: 30px;
+}
+`
+
+StyledApp.displayName = 'App'
+
+export default StyledApp
+
---
+++ src/components/List.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const icons_clock = require('../images/icons_clock.png')
+const icons_phone = require('../images/icons_phone.png')
+const icons_pin = require('../images/icons_pin.png')
+const icons_tag = require('../images/icons_tag.png') 
+
+const List = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default List
