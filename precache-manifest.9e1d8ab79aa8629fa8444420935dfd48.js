87a59d7c5becd4a16f8f06c86301789b98c9b97b
Nien <wolfzxcv@gmail.com>
2019-07-08 21:41:26 +0100

    Updates

---
+++ precache-manifest.9e1d8ab79aa8629fa8444420935dfd48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd4e64d7fba5031bcb23855cf6d3b74e",
+    "url": "/React-Kaohsiung-Travel-Guide/index.html"
+  },
+  {
+    "revision": "639f049707eaddf66609",
+    "url": "/React-Kaohsiung-Travel-Guide/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "468b054b1205b86b18e1",
+    "url": "/React-Kaohsiung-Travel-Guide/static/js/2.1c541c1e.chunk.js"
+  },
+  {
+    "revision": "639f049707eaddf66609",
+    "url": "/React-Kaohsiung-Travel-Guide/static/js/main.a6e7e79c.chunk.js"
+  },
+  {
+    "revision": "bd3787ead271a91a906a",
+    "url": "/React-Kaohsiung-Travel-Guide/static/js/runtime~main.8f05823d.js"
+  },
+  {
+    "revision": "e66e6a7c8dd273f485f6b11d374de442",
+    "url": "/React-Kaohsiung-Travel-Guide/static/media/header_bg.e66e6a7c.png"
+  }
+]);
